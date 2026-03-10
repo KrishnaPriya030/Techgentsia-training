b1ab95dae77dccc05de3a3f0fa0c1b1f3c26bace
KrishnaPriya030 <krishnapriyas030@gmail.com>
2026-03-10 20:04:56 +0530

    Added drumkit

---
+++ script.js
@@ -0,0 +1,25 @@
+const sounds = {
+    btn1: "sounds/deandre_aaron-punk-drum-kit-sound-no1-425687.mp3",
+    btn2: "sounds/edr-synth-side-stick-471670.mp3",
+    btn3: "sounds/freesound_community-drum-beat-02-36276.mp3",
+    btn4: "sounds/freesound_community-soda-bottle-base-drum-46019.mp3",
+    btn5: "sounds/soundreality-beat-of-time-128705.mp3",
+    btn6: "sounds/west_tunes_thebeatchef-my-snare-2-243040.mp3",
+    btn7: "sounds/fronbondi_skegs-drum-rock-style-drumbreak-01-145bpm-3bars-462984.mp3",
+    btn8: "sounds/freesound_community-drum-kit-100612.mp3"
+};
+
+for (let key in sounds) {
+
+    document.getElementById(key).addEventListener("click", function () {
+
+        const audio = new Audio(sounds[key]);
+        audio.play();
+        setTimeout(() => {
+    audio.pause();
+    audio.currentTime = 0;
+}, 1000);
+
+    });
+
+}
